a030b23473c227e72d6ffd49e855c5d26d892299
Oleksii <alexey.kovbel@gmail.com>
2020-04-03 23:56:35 +0300

    veersion 0.2.2

--- commands/test.js
+++ commands/test.js
@@ -1,6 +1,7 @@
 module.exports.run = async (client, message, args, r, conn, config) => {
   r.db(config.db).table("servers").get(message.guild.id).run(conn, function (err, serv) {
-    console.log(message.guild.members.cache.find(m => m.id === message.author.id)._roles.some(el => el === serv.server.admin_role));
+    var a = args.filter(s => s !== '')
+    console.log(a[0]);
   })
 };
 
